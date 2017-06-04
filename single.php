b90ae72c5d3da9dd73faec64ee70f73104cd60f1
棒棒勝 <jayuiop@gmail.com>
2017-06-04 15:18:39 +0800

    解決隨機圖片無法顯示的問題

--- single.php
+++ single.php
@@ -5,7 +5,7 @@
 			
 			<div class="ts vertically very padded left aligned slate post">
 				<div class="image">
-					<script src="https://cdn.gnehs.com.tw/cdn/randompicture.js"></script>   
+					<script src="//rawgit.com/gnehs/RandomPic/master/randompicture.js"></script>   
 				</div>
 				<span class="header"><?php the_title(); ?></span>
 				<span class="description">
