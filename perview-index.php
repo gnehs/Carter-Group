0b3252ed83301ae4cbe3c8cf05579711ce40f084
棒棒勝 <jayuiop@gmail.com>
2017-08-07 13:51:27 +0800

    加上新版特性，刪減部分不需要檔案

---
+++ perview-index.php
@@ -0,0 +1,32 @@
+<?php $category_id_ch = get_cat_ID('頻道'); ?>
+<?php $category_id_group = get_cat_ID('群組'); ?>
+<?php $category_id_info = get_cat_ID('公告'); ?>
+<?php $category_id_site= get_cat_ID('網站'); ?>
+<?php if ( in_category($category_id_ch) ) { ?>
+	<div class="ts inverted ch card">
+<?php }elseif ( in_category($category_id_group) ) { ?>
+	<div class="ts inverted group card">
+<?php }elseif ( in_category($category_id_info) ) { ?>
+	<div class="ts inverted info card">
+<?php }elseif ( in_category($category_id_site) ) { ?>
+	<div class="ts inverted site card">
+<?php } else { ?>
+	<div class="ts inverted card">
+<?php } ?>
+	<div class="content">
+		<div class="header"><?php the_title(); ?></div>
+		<div class="meta">
+			<a><?php the_time('Y年n月j日') ?></a>
+			<a><?php edit_post_link('編輯', ''); ?></a>
+			<a><?php the_category(‘,’) ?></a>
+		</div>
+		<div class="description">
+			<?php the_excerpt(); ?>
+		</div>
+	</div>
+	<div class="ts fluid bottom attached buttons post operation">
+		<a class="ts opinion button" href="<?php echo get_post_meta($post->ID, $prefix . 'tg-link', true); ?>">加入</a>
+		<a class="ts opinion button click load" href="<?php the_permalink(); ?>">更多資訊</a>
+		<?php edit_post_link( __( 'Edit', 'Carter' ), '', '', '','ts opinion button click load' ); ?> 
+	</div>
+</div>
--- single.php
+++ single.php
@@ -2,24 +2,14 @@
 	<div class="ts narrow container" style="padding-top: 20px;"><div class="ts stackable grid">
 		<?php if (have_posts()) : the_post(); update_post_caches($posts); ?>
 		<div class="twelve wide column">
-			
-			<div class="ts vertically very padded left aligned slate post">
+			<div class="ts heading vertically padded slate attached post">
 				<div class="image">
-					<script src="//gnehs.github.io/RandomPic/randompicture.js"></script>   
+					<script src="//gnehs.github.io/RandomPic/randompicture.js"></script>  
 				</div>
 				<span class="header"><?php the_title(); ?></span>
-				<span class="description">
-					<?php the_time('Y年n月j日') ?>
-					<?php comments_popup_link('還沒有人留言', '1 則留言', '% 則留言', '', '已關閉留言'); ?>
-					<?php edit_post_link('編輯', ''); ?>
-				</span>
-				<span class="description">
-					<?php the_category(' ') ?>
-				</span>
-				<div class="action" id="PostOperation">
-					<a class="ts inverted basic button" href="<?php echo get_post_meta($post->ID, $prefix . 'tg-link', true); ?>">加入</a>
-				</div>						  
+				<span class="description"><?php the_time('Y/n/j') ?> <?php edit_post_link(__( 'Edit', 'Carter' ), ''); ?><?php the_category(' ') ?></span>
 			</div>
+			<a class="ts fluid bottom attached button" href="<?php echo get_post_meta($post->ID, $prefix . 'tg-link', true); ?>">加入</a>
 			
 			<div class="ts hidden divider"></div>
 			
